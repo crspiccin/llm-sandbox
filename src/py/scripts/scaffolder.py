f8e618df481ea9e864e314f37deb6a28de5ee3c0
Cristiano E.Piccin <crspiccin@gmail.com>
2023-06-12 13:10:51 -0300

    config on scaffolder

--- src/py/scripts/scaffolder.py
+++ src/py/scripts/scaffolder.py
@@ -10,24 +10,26 @@ from langchain.prompts import (
 from langchain.schema import (
     SystemMessage
 )
+import json
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 load_dotenv(dotenv_path=Path("../.env"))
 
-import argparse
- 
-parser = argparse.ArgumentParser(description="Just an example",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-a", "--archive", action="store_true", help="archive mode")
-parser.add_argument("-v", "--verbose", action="store_true", help="increase verbosity")
-args = parser.parse_args()
+
+def load_config():
+    with open('config.json', 'r') as file:
+        config_file = json.load(file)
+        print(json.dumps(config_file, indent=4))
+        return config_file
+
+
 
 
 
 def run_chat_model():
     print('starting the model ')
-    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
 
     system_template = "Act as a seasoned senior software developer specialist in {language}"
     human_template = "Create a {entity} entity following hexagonal architecture with services layer covered by unit test including operations like create, update, read and delete following SQL ANSI on a repository layer, and expose it on adapter layer by a {api_type} api."
@@ -41,31 +43,26 @@ def run_chat_model():
     content = chat(messages).content
     return content
 
-def run_llm_chain_model():
-    print('starting the llm model ')
-    llm = OpenAI(temperature=0.7)
-    chat = ChatOpenAI(temperature=0.7)
-
-    prompt_template = """"
-        Act as a senior software developer specialist in {language} and following the code conventions , 
-        create a {entity} entity microservice following hexagonal architecture frameworks with services layer including operations like create, update, read and delete and following SQL ANSI on a repository layer, expose it on adapter layer by a {api_type} api,
-        create unit test for the services layer,
-        create the build file with the dependencies necessary to build the service,
-        On each file put in the first line the word BREAK, on the second line the file name and on the remaining lines the source body 
-    """
+def run_llm_chain_model(config_file):
+    print('Starting the llm model ...')
+    # llm = OpenAI(temperature=config_file['temperature'])
+    chat = ChatOpenAI(temperature=config_file['temperature'])
 
     prompt = PromptTemplate(
-        template = prompt_template,
+        template = config_file['prompt_template'],
         input_variables=["language", "entity", "api_type"]
     )
 
     chain = LLMChain(llm=chat, prompt=prompt)
 
-    return chain.run({"language":"typescript", "entity":"Payment", "api_type":"REST"})
+    result = chain.run({"language":config_file['language'], "entity":config_file['entity'], "api_type":config_file['api_type']})
+    print(result)
+    return result
 
 output_dir =  "../../../output/"
 
-def output_files(llm_output):
+def output_files(llm_output, output_dir):
+    os.makedirs(os.path.dirname(output_dir), exist_ok=True)
     files = llm_output.split("BREAK")
     for file in files:
         tokens = file.split("\n")
@@ -73,13 +70,13 @@ def output_files(llm_output):
             if token != '':
                 print(token)
                 # print(file.replace(token, ""))
-                with open(output_dir + token, 'w+') as fp:
+                with open(os.path.join(output_dir, token) , 'w+') as fp:
                     fp.write(file.replace(token, ""))
                     pass
                 break
 
 
-
-output_files(run_llm_chain_model())
+config_file = load_config()
+output_files(run_llm_chain_model(config_file), config_file['output_dir'])
 
 
